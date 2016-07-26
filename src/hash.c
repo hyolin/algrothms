56bfcd8659e90ac7c1521ec0c88c4eca3931d04f
hyolin <hyolin.han@gmail.com>
2016-07-26 21:35:46 +0800

    add hash murmurhash2, int hash and case intensive hash

--- src/hash.c
+++ src/hash.c
@@ -3,7 +3,70 @@
 	> Author: hyolin 
 	> Created Time: Mon 19 Jan 2015 02:19:03 PM CST
  ************************************************************************/
+#include <stdint.h>
+#include <ctype.h>
 #include "hash.h"
+
+static uint32_t __hash_seed = 5381;
+
+/** Thomas wang's hash function */
+unsigned int _hash_int_func(unsigned int key)
+{
+    key += ~(key << 15);
+    key ^= (key >> 10);
+    key += (key << 3);
+    key ^= (key >> 6);
+    key += ~(key << 11);
+    key ^= (key >> 16);
+    return key;
+}
+
+
+unsigned int _hash_case_func(const unsigned char *buf, int len)
+{
+    unsigned int hash = (unsigned int) __hash_seed;
+    while (len--) {
+        hash = ((hash << 5) + hash + tolower(*buf++));
+    }
+    return hash;
+}
+/** Murmurhash2 by Austin Appleby */
+unsigned int _hash_murmur_func(const void *key, len)
+{
+    uint32_t seed = __hash_seed;
+    const uint32_t m = 0x5bd1e995;
+    const int r = 24;
+
+    uint32_t h = seed ^ len;
+    /** Max 4 byte into hash */
+    const unsigned char *data = (const unsigned char *) key;
+    while (len > 4) {
+        uint32_t k = *(uint32_t *)data;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h *= m;
+        h ^= k;
+
+        data += 4;
+        len -= 4;
+    }
+
+    /** handle last few bytes */
+    switch (len) {
+    case 3: h ^= data[2] << 16;
+    case 2: h ^= data[1] << 8;
+    case 1: h ^= data[0]; h *= m;
+    }
+
+    h ^= h >> 13;
+    h *= m;
+    h ^= h >> 15;
+
+    return (unsigned int h);
+}
 
 /* hashtbl_init */ 
 int hashtbl_init(HashTbl *htbl, int buckets,
